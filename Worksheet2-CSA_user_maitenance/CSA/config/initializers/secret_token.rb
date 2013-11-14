bde20c41b236501f464820745e3b48299fa08ea8
Martin Key <mkey@live.co.uk>
2013-11-14 16:47:40 +0000

    Worksheet 2 complete

---
+++ Worksheet2-CSA_user_maitenance/CSA/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CSA::Application.config.secret_key_base = 'cab43b5327f6e2f057042604937cd4e1704a0223dd786c81e0c5ac37fa46847862d117fe2e22efcc9374cb8264aeac9a86403d3c1b3f9b818e06304df64be2f0'
---
+++ Worksheet2-CSA_user_maitenance/CSA/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CSA::Application.config.session_store :cookie_store, key: '_CSA_session'
