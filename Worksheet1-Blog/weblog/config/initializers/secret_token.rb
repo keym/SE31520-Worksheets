f705a7a8729e60c5250f984f8a3894c9416a4528
Martin Key <mkey@live.co.uk>
2013-11-14 16:03:33 +0000

    Worksheet 1 Complete

---
+++ Worksheet1-Blog/weblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weblog::Application.config.secret_key_base = '6b5ca587fe54fb54f3a62be5cf6947f9c5b5641851008bcf2a4b3c6841d6bf700f76c4ad5dfe18ff06e585acf9a801297fe45dd0832e6a6aad8725d1de4d81d7'
